d9111a3eae8d9bcbfcca2271fe6500e617d584a2
Honey1321 <honeykeerthana2004@gmail.com>
2026-09-19 22:50:01 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,2 +1,22 @@
-name = "Honey"
-print(len(name))
+class String:
+  def text(self):
+    text = input("Enter a string:")
+    
+    self.upper = text.upper()
+    self.lower = text.lower()
+    self.length = len(text)
+    self.slice = text[2:7]
+    self.strip = text.strip()
+    self.count = text.count("o")
+    self.find = text.find("good")
+    
+s = String()
+s.text()
+
+print("Upper:",s.upper)
+print("Lower:",s.lower)
+print("Length of the string:",s.length)
+print("Slicing:",s.slice)
+print("Removing space:",s.strip)
+print("The number of 'o' present in the string is",s.count)
+print("Found at the position ",s.find)
